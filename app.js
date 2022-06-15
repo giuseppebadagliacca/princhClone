66e958494a5bb77247c01c6c710472dc6da62703
Giuseppe Badagliacca <joebadagliacca@gmail.com>
2022-06-15 11:10:15 -0400

    6/15/22

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+let printID = []
 function jump(feild, move){
-  console.log(feild.value.length, feild.maxLength)
   if(feild.value.length >= feild.maxLength){
     document.getElementById(move).focus()
   }
+  printID.push(feild.value);
+}
+
+function moveOn(){
+  const body =  document.querySelector('body');
+  body.style.textAlign = 'center'
+  body.style.marginTop = '40px'
+  body.innerHTML =
+  `
+  <h1>Job Number:${printID.join('')} has been sent to print</h1>
+
+
+  `
 }
